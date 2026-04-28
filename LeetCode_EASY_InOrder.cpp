db84e325f761633af8e0f09d01fe2515d8b833b9
L-Mercado <frozenreality14@gmail.com>
2026-04-28 01:59:01 -0400

    Update LeetCode_EASY_InOrder.cpp

--- LeetCode_EASY_InOrder.cpp
+++ LeetCode_EASY_InOrder.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <vector>
+#include <stack>
 using namespace std;
 
 struct TreeNode {
@@ -16,27 +17,31 @@ class Solution {
 public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> result;
-        traverse(root, result);
-        return result;
-    }
-
-private:
-    void traverse(TreeNode* node, vector<int>& result) {
+        stack<TreeNode*> stack;
+        TreeNode* current = root;
         
-        if (node == nullptr) return;
-
-        traverse(node->left, result);
-
-        result.push_back(node->val);
-
-        traverse(node->right, result);
+        while (current != nullptr || !stack.empty()) {
+            while (current != nullptr) {
+                stack.push(current);
+                current = current->left;
+            }
+            
+           
+            current = stack.top();
+            stack.pop();
+            result.push_back(current->val);
+            
+            current = current->right;
+        }
+        
+        return result;
     }
 };
 
 int main() {
     Solution sol;
-
     
+  
     TreeNode* root = new TreeNode(4);
     root->left = new TreeNode(2);
     root->right = new TreeNode(6);
@@ -44,18 +49,18 @@ int main() {
     root->left->right = new TreeNode(3);
     root->right->left = new TreeNode(5);
     root->right->right = new TreeNode(7);
-
+    
     cout << "Tree:\n    4\n   / \\\n  2   6\n / \\ / \\\n1  3 5 7\n" << endl;
-
+    
     vector<int> result = sol.inorderTraversal(root);
-
+    
     cout << "Inorder Count: [";
     for (size_t i = 0; i < result.size(); i++) {
         cout << result[i];
         if (i < result.size() - 1) cout << ", ";
     }
     cout << "]" << endl;
-
-   
+    
     return 0;
+}
 }
